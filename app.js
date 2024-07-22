d1bd3564fab4d5684451facae91b3f46c80ae11f
Jonathan Wong <Xmexybutt@gmail.com>
2024-07-22 14:56:37 -0400

    Updated content and made projects more dynamic and increase scalability

--- app.js
+++ app.js
@@ -1,32 +1,28 @@
-const menu = document.querySelector('#mobile-menu');
-const menuLinks = document.querySelector('.navbar__menu');
+const menu = document.querySelector("#mobile-menu");
+const menuLinks = document.querySelector(".navbar__menu");
 
-menu.addEventListener('click', function(){
-    menu.classList.toggle('is-active');
-    menuLinks.classList.toggle('active');
+menu.addEventListener("click", function () {
+  menu.classList.toggle("is-active");
+  menuLinks.classList.toggle("active");
 });
 
-function afterShowAni(entries){
-    entries.forEach((entry)=>{
-        entry.target.classList.add('projects__card_afterShow');
-    })
+function afterShowAni(entry) {
+  entry.target.classList.add("projects__card_afterShow");
 }
 
-const observer = new IntersectionObserver((entries) =>{
-    entries.forEach((entry) => {
-        console.log(entry)
-        if (entry.isIntersecting){
-            entry.target.classList.add('show');
-            setTimeout(function() {
-                afterShowAni(entries);
-            }, 1200);
-        }
-        // else
-        // {
-        //     entry.target.classList.remove('show');
-        // }
-    })
-})
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    console.log(entry);
+    if (entry.isIntersecting) {
+      entry.target.classList.add("show");
+      if (entry.target.classList.contains("show")) {
+        setTimeout(function () {
+          afterShowAni(entry);
+        }, 1200);
+      }
+    }
+  });
+});
 
-const hiddenElements = document.querySelectorAll('.hidden');
+const hiddenElements = document.querySelectorAll(".hidden");
 hiddenElements.forEach((el) => observer.observe(el));
